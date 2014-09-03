f40b9474d43ee0fe3d316227a0202900d8176275
tliz <7winny@gmail.com>
2014-09-04 00:19:53 +0300

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", :type => :request do
-  describe "GET /static_pages" do
+  #describe "GET /static_pages" do
     #it "works! (now write some real specs)" do
-     # get static_pages_index_path
+      #get static_pages_index_path
       #expect(response.status).to be(200)
 
-      it "should have the content 'Sample App'" do
-      		getsx static_pages_index_path
-      		expect(response.status).to be('Sample App')
-      	#visit '/static_pages/home'
-      	#expect(page).to have_content('Sample App')
+  describe "Home page" do
 
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Tliz | Home")
+    end
+
   end
+
+  #test that Help Page exists
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Tliz | Help")
+    end
+
+  end
+
+  #test that About Page exists
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Tliz | About Us")
+    end
+
+  end
+  
 end
